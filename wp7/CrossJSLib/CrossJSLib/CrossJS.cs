46f6b77c5ff21a63da1bafeea91b70e5108bb2d8
Ernane Lopes <ernane@rebornit.co>
2014-12-18 17:36:06 -0200

    (WP8) Implementação da ponte nativo/js

---
+++ wp7/CrossJSLib/CrossJSLib/CrossJS.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossJSLib
+{
+    public class CrossJS
+    {
+        static CrossJS singleInstance;
+
+        public static CrossJS Instance
+        {
+            get
+            {
+                if (singleInstance == null)
+                {
+                    singleInstance = new CrossJS();
+                }
+
+                return singleInstance;
+            }
+        }
+        private CrossJS() { }
+
+        public void LoadJSFile(string filePath)
+        {
+
+        }
+
+        public void SetJSVariable(string variablePath, object nativeValue)
+        {
+
+        }
+
+        public object ExecuteJS(string jsCode)
+        {
+            return null;
+        }
+    }
+}
